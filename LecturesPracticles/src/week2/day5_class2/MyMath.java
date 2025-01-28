e6af30596066e3c667b78529e13a3cf1615d3f7b
Krishna Aryal <info.karyal@gmail.com>
2025-01-28 08:45:02 +0545

    28 JAN 2025 8:44

--- LecturesPracticles/src/week2/day5_class2/MyMath.java
+++ LecturesPracticles/src/week2/day5_class2/MyMath.java
@@ -1,6 +1,24 @@
 package week2.day5_class2;
 
 public class MyMath {
+	
+	public void calculateDiv() {
+		//calculate qnt of two numbers
+		int n1, n2, n3;
+		n1=10;
+		n2=3;
+		n3=n1/n2;
+		System.out.println("Qnt :"+n3);
+	}
+	public void calculatePrd() {
+		//calculate product of two numbers
+		int n1, n2, n3;
+		n1=78;
+		n2=3;
+		n3=n1*n2;
+		System.out.println("Prd : "+n3);
+	}
+	
 	public void calculateSum() {
 		//calculate sum of two numbers
 		int n1, n2, n3;
@@ -18,5 +36,7 @@ public class MyMath {
 		n3=n1-n2;
 		System.out.println("Diff : "+n3);
 	}
+	
+	
 	
 }
---
+++ LecturesPracticles/src/week3/day2/MyMathTest.java
@@ -0,0 +1,14 @@
+package week3.day2;
+
+import week2.day5_class2.MyMath;
+
+public class MyMathTest {
+
+	public static void main(String[] args) {
+		MyMath myMath = new MyMath();
+		myMath.calculatePrd();
+		myMath.calculateDiff();
+		myMath.calculatePrd();
+		myMath.calculateDiv();
+	}
+}
