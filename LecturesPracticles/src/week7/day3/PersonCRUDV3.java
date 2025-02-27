0dd808c55552909ee75a7f1f7372c30d5c86ab7f
Krishna Aryal <info.karyal@gmail.com>
2025-02-27 10:18:03 +0545

    27 Feb 2025 10:17 AM

--- LecturesPracticles/src/week7/day3/PersonCRUDV3.java
+++ LecturesPracticles/src/week7/day3/PersonCRUDV3.java
@@ -2,6 +2,7 @@ package week7.day3;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 //persons(pid:int, full_name varchar(50), email varcahr(50))
 
@@ -14,6 +15,7 @@ public class PersonCRUDV3 {
 		final String DBNAME = "mydb";
 		final String DRIVER = "com.mysql.cj.jdbc.Driver";
 		final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DBNAME;
+		
 		// Declare
 		Connection conn;
 		try {
@@ -27,4 +29,51 @@ public class PersonCRUDV3 {
 			System.out.println("Error : " + ex.getMessage());
 		}
 	}
+	
+	public void insert(int pid, String fullName, String email) {
+		//Insert Record (values from parameter)
+		final String HOST = "localhost";
+		final String USER = "root";
+		final String PASS = "pcps@123";
+		final int PORT = 3306;
+		final String DBNAME = "mydb";
+		final String DRIVER = "com.mysql.cj.jdbc.Driver";
+		final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DBNAME;
+		// Declare
+		Connection conn;
+		String sql="INSERT INTO persons(pid, full_name, email) VALUES(?, ?, ?)";
+		PreparedStatement pStat;
+		try {
+			// input, process, output
+			conn = DriverManager.getConnection(URL, USER, PASS);// Connect with db server
+			System.out.println("Connected!"); // message to user
+			pStat = conn.prepareStatement(sql);
+			pStat.setInt(1, pid); //Set value
+			pStat.setString(2, fullName); //Set value
+			pStat.setString(3, email); //Set value
+			pStat.executeUpdate(); //Run Query
+			pStat.close();
+			System.out.println("Insert record successfully");
+			conn.close(); // Close Connection
+			System.out.println("Connection Closed!");// message to user
+		} catch (Exception ex) {
+			// error message
+			System.out.println("Error : " + ex.getMessage());
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- LecturesPracticles/src/week7/day3/Test.java
+++ LecturesPracticles/src/week7/day3/Test.java
@@ -1,13 +1,24 @@
 package week7.day3;
 
+//persons(pid:int, full_name varchar(50), email varcahr(50))
 public class Test {
 
 	public static void main(String[] args) {
 		//new PersonCRUDV2().connect();
-		new PersonCRUDV2().insert();
+		//new PersonCRUDV2().insert();
 		//new PersonCRUDV2().all();
 		//new PersonCRUDV2().searchPID();//?
 		//new PersonCRUDV2().update();//?
 		//new PersonCRUDV2().delete();//?
+		
+		//PersonCRUDV3 Test
+		//Connect with database
+		new PersonCRUDV3().connect();
+		
+		//Insert Record
+		int pid=9;
+		String fullName="Sudan Rai";
+		String email="sudan@gmail.com";
+		
 	}
 }
